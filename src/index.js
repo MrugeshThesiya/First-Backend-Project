acb1aabc0b3dfa54625c4fc1df397d08f16a2b7a
Mrugesh Thesiya <mrugeshthesiya16@gmail.com>
2024-05-16 14:22:10 +0530

    utils code added

--- src/index.js
+++ src/index.js
@@ -6,6 +6,21 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+
+    app.on("error",(error)=>{
+        console.log("Error : " , error);
+        throw error
+    })
+
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+    
+})
+.catch((error)=>{
+    console.log("MongoDb Connection failed !! " , error)
+})
 
 
 
